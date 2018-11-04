dc28bf59b1f0027a1b0c338e2094dd6ed5946c0f
tshemsedinov <timur.shemsedinov@gmail.com>
2018-11-04 15:54:51 +0200

    Chaining example

---
+++ JavaScript/7-chaining.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Imperative
+
+class Adder {
+  constructor(initial) {
+    this.value = initial;
+  }
+  add(value) {
+    this.value += value;
+    return this;
+  }
+  valueOf() {
+    return this.value;
+  }
+}
+
+const sum1 = new Adder(1).add(9).add(1).add(7);
+console.log('Sum:', +sum1);
+
+// Functional
+
+const adder = initial => Object.assign(
+  value => adder(initial + value),
+  { valueOf: () => initial }
+);
+
+const sum2 = adder(1)(9)(1)(7);
+console.log('Sum:', +sum2);
+
+// Functional
+
+const add = initial => ({
+  add: value => add(initial + value),
+  valueOf: () => initial
+});
+
+const sum3 = add(1).add(9).add(1).add(7);
+console.log('Sum:', +sum3);
